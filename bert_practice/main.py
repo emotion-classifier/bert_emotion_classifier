7de5d372089f08013976c9b0f04e7b842955c631
sentiment <lionwithnomane@gmail.com>
2019-09-15 16:18:44 +0900

    remove practice code

--- bert_practice/main.py
+++
@@ -1,8 +0,0 @@
-import LSTM_Model
-
-model = LSTM_Model.LSTM_Model()
-
-#model.training(epo=10000, continue_training=False, l2_norm=True, first_training=True)
-
-model.keep_prob = 1.0
-model.evaluation()
