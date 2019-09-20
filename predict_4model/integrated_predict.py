181e5227b575394a24f3a4a2ec32d7f0fec62830
DrinkingMilktea <38062461+DrinkingMilktea@users.noreply.github.com>
2019-09-21 07:35:27 +0900

    old file remove

--- predict_4model/integrated_predict.py
+++ predict_4model/integrated_predict.py
@@ -1,5 +1,5 @@
 import naive_bayes_api
-import support_vector_api
+import support_vector_api_update
 import biLSTM_api
 import bert_api
 from konlpy.tag import Okt
@@ -13,7 +13,7 @@ while True:
     print("트윗을 입력해주세요 : ", end="")
     input_text = input()
     naive_bayes = naive_bayes_api.call_api(input_text)
-    svm = support_vector_api.call_api(input_text)
+    svm = support_vector_api_update.call_api(input_text)
     bilstm = biLSTM_api.call_api(input_text)
     bert = bert_api.call_api(input_text)
     print(input_text)
