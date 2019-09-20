877479daad66b8f3b7a6964ecb4c348c3882c022
dlaekdud88 <dlaekdud88@gmail.com>
2019-09-20 18:42:28 +0900

    no message

--- predict_4model/support_vector_api.py
+++ predict_4model/support_vector_api.py
@@ -1,32 +1,29 @@
 import pandas as pd
 import numpy as np
 import pickle
-from sklearn.model_selection import train_test_split
 from sklearn.externals import joblib
 from konlpy.tag import Okt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
-import matplotlib.pyplot as plt
 """ SVM API """
 
-def get_noun(text):
+def get_morphs(text):
     tokenizer = Okt()
-    nouns = tokenizer.nouns(text)
-    return [n for n in nouns]
+    morphs = tokenizer.morphs(text)
+    return [n for n in morphs]
 
 def support_vector_api(tweet):
     print("로딩중입니다")
-    mod = joblib.load('svm_model.pkl')
-    tmp = mod.predict([tweet])
-
-    result = tmp[0]
-    return result
+    with open('support_vector_ver2.model', 'rb') as f:
+        text_clf_svm = pickle.load(f)
+    return text_clf_svm.predict([tweet])[0]
 
 def call_api(tweet):
     val_predict = support_vector_api(tweet)
     ret = ""
+
     if val_predict == 1:
         ret = "기쁨"
     elif val_predict == 2:
@@ -37,7 +34,5 @@ def call_api(tweet):
         ret = "중립"
     else:
         ret = str(val_predict[0]) + "잘못된 분류값입니다."
-    return ret
 
-if __name__ == "__main__":
-    pass
+    return ret
