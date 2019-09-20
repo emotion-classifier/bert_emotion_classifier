b22348bf6fefdecf4be991d62f977134dfaf4470
DrinkingMilktea <38062461+DrinkingMilktea@users.noreply.github.com>
2019-09-21 07:21:46 +0900

    rm old files

--- predict_4model/integrated_predict.py
+++ predict_4model/integrated_predict.py
@@ -1,19 +1,19 @@
 import naive_bayes_api
-import API_SVM_ver2
+import support_vector_api
 import biLSTM_api
 import bert_api
 from konlpy.tag import Okt
 
-def get_noun(text):
+def get_morphs(text):
     tokenizer = Okt()
-    nouns = tokenizer.nouns(text)
-    return [n for n in nouns]
+    morphs = tokenizer.morphs(text)
+    return [n for n in morphs]
 
 while True:
     print("트윗을 입력해주세요 : ", end="")
     input_text = input()
     naive_bayes = naive_bayes_api.call_api(input_text)
-    svm = API_SVM_ver2.call_api(input_text)
+    svm = support_vector_api.call_api(input_text)
     bilstm = biLSTM_api.call_api(input_text)
     bert = bert_api.call_api(input_text)
     print(input_text)
